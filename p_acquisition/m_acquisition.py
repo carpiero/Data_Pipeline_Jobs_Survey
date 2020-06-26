163d3a54d6450eadc529561930aadc27fa4685e7
carpiero <carpiero@gmail.com>
2020-06-26 20:45:20 +0200

    start pipeline

--- p_acquisition/m_acquisition.py
+++ p_acquisition/m_acquisition.py
@@ -1,7 +1,44 @@
 import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+from functools import reduce
 
 # acquisition functions
 
+def get_tables(path):
+    engine = create_engine(f'sqlite:///{path}')
+    df_personal = pd.read_sql("SELECT * FROM personal_info" , engine)
+    df_country = pd.read_sql("SELECT * FROM country_info" , engine)
+    df_career = pd.read_sql("SELECT * FROM career_info" , engine)
+    df_poll = pd.read_sql("SELECT * FROM poll_info" , engine)
+    dfs = [df_personal , df_country , df_career , df_poll]
+    df_final = reduce(lambda left , right: pd.merge(left , right , on='uuid') , dfs)
+    return df_final
+
+def clean(dataframe):
+    dataframe_clean = dataframe
+
+    return dataframe_clean
+
+
+
+
 def acquire():
-    data = pd.read_csv('./data/raw/vehicles.csv')
-    return data
+    return clean(get_tables('./data/raw/raw_data_project_m1.db'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
