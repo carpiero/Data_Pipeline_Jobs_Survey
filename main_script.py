89b338472c9be555cf9bd9f08ce987fdc683d2c5
carpiero <carpiero@gmail.com>
2020-07-01 22:07:44 +0200

    01.07.2020v02

--- main_script.py
+++ main_script.py
@@ -5,21 +5,23 @@ from p_analysis import m_analysis as man
 from p_reporting import m_reporting as mre
 
 def argument_parser():
+
     parser = argparse.ArgumentParser(description = 'Set country')
-    parser.add_argument("-c", "--country", help="specify country for the results", type=str)
+    parser.add_argument("-c", "--country", help="specify country for the results, default= All the countries", type=str)
     parser.add_argument("-p" , "--path" , help="specify the path of the database, the file .db" , type=str,required=True)
-    parser.add_argument("-u" , "--unemployed" , help="specify the results with Unemployed or Part time Job or Inactive effect, must be yes or no, default=yes" , type=str)
+    parser.add_argument("-u" , "--unemployed" , choices=['yes','no'],help="specify the results with Unemployed or Part time Job or Inactive effect, must be yes or no, default=yes" , type=str)
     args = parser.parse_args()
     return args
+
 
 def main(arguments):
 
     data = mac.acquire(arguments.path)
     filtered = mwr.wrangle(data , arguments.unemployed)
     results = man.analyze(filtered)
-    results.to_csv('./data/results/df_results.csv')
-
     reporting = mre.reporting(results, arguments.country)
+
+    reporting.to_csv('./data/results/Results.csv')
 
     print(reporting)
 
@@ -38,6 +40,6 @@ if __name__ == '__main__':
 
     except NameError:
 
-        print('\n=================================================================================================\n\nERROR: Please insert a correct Country of the list inside ./data/results/country_list.csv\n\n=================================================================================================\n\n')
+        print('\n=================================================================================================\n\nERROR: Please insert a correct Country of the list above or inside ./data/results/country_list.csv\n\n=================================================================================================\n\n')
 
 
--- p_reporting/m_reporting.py
+++ p_reporting/m_reporting.py
@@ -19,8 +19,32 @@ def specific_country(results,country):
             country = country.title()
 
             if country not in country_list:
-                raise NameError
+                print('\n=================================================================================================\n')
 
+                if len(country_list) % 4 == 0:
+                    for i in range(0 , len(country_list) - 1 , 4):
+                        print(
+                            f'{country_list[i]:<15}{country_list[i + 1]:<15}{country_list[i + 2]:<15}{country_list[i + 3]:<15}')
+
+                elif len(country_list) % 4 == 1:
+                    for i in range(0 , len(country_list) - 1 , 4):
+                        print(
+                            f'{country_list[i]:<15}{country_list[i + 1]:<15}{country_list[i + 2]:<15}{country_list[i + 3]:<15}')
+                        print(country_list[-1])
+
+                elif len(country_list) % 4 == 2:
+                    for i in range(0 , len(country_list) - 2 , 4):
+                        print(
+                            f'{country_list[i]:<15}{country_list[i + 1]:<15}{country_list[i + 2]:<15}{country_list[i + 3]:<15}')
+                        print(f'{country_list[-2]:<15}{country_list[-1]:<15}')
+
+                else:
+                    for i in range(0 , len(country_list) - 3 , 4):
+                        print(
+                            f'{country_list[i]:<15}{country_list[i + 1]:<15}{country_list[i + 2]:<15}{country_list[i + 3]:<15}')
+                        print(f'{country_list[-3]:<15}{country_list[-2]:<15}{country_list[-1]:<15}')
+
+                raise NameError
 
             else:
                 print(f'\n\n============================ Below you could see the table of content of {country}   ====================================\n\n')
@@ -45,31 +69,3 @@ def reporting(results,country):
         return specific_country(results,country)
 
 
-# if country == None:
-    #     print('\n\n============================ Below you could see the table of content with all the countries  ====================================\n\n')
-    #
-    #     return results
-    #
-    # elif country not in country_list:
-    #     country = country.lower()
-    #     country = country.title()
-    #     if country not in country_list:
-    #
-    #         return '\n=================================================================================================\n\nError, please insert a correct Country of the list inside ./data/results/country_list.csv\n\n=================================================================================================\n\n'
-    #
-    #     else:
-    #         print(f'\n\n============================ Below you could see the table of content of {country}   ====================================\n\n')
-    #
-    #         df_specific_country = results.loc[results['Country'] \
-    #             .str.contains(country)].sort_values(by='Quantity' , ascending=False)
-    #
-    #         return df_specific_country
-    #
-    #
-    # else:
-    #     print(f'\n\n============================ Below you could see the table of content of {country}   ====================================\n\n')
-    #
-    #     df_specific_country = results.loc[results['Country'] \
-    #         .str.contains(country)].sort_values(by='Quantity' , ascending=False)
-    #
-    #     return df_specific_country
